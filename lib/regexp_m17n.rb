2f5c539b668f9725d4f948aa4e4f4ef44749cc38
Paul B <paul@bonaud.fr>
2014-08-01 15:45:45 +0200

    Playing with regexp match encoding compat

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,5 +1,23 @@
+class String
+  alias_method :encode_old, :encode
+
+  def encode(enc)
+    begin
+      self.encode_old(enc)
+    rescue Encoding::ConverterNotFoundError
+      puts "No converter shipped for #{enc}"
+      '.'
+    end
+  end
+end
+
 module RegexpM17N
   def self.non_empty?(str)
-    str =~ /^.+$/
+    begin
+      str =~ /^.+$/
+    rescue Encoding::CompatibilityError
+      puts "Regex.match incompatible with encoding #{str.encoding}"
+      str.encode('UTF-8') =~ /^.+$/
+    end
   end
 end
